d39b2551432747f0809487a87d1ca636a7609e03
Shivakumar R <shivakumar.r@nocodez.com>
2023-04-09 16:43:10 +0530

    Update UI to add description and animate list item

--- app/src/main/java/com/test/mealz/ui/screens/MealsCategoryScreen.kt
+++ app/src/main/java/com/test/mealz/ui/screens/MealsCategoryScreen.kt
@@ -1,15 +1,20 @@
 package com.test.mealz.ui.screens
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -51,26 +56,56 @@ fun MealCategory(meal: MealsResponse) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row {
-            AsyncImage(
-                model = meal.imageUrl,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(90.dp)
-                    .padding(4.dp)
-            )
-            Column(
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .padding(16.dp)
-            ) {
-                Text(
-                    text = meal.name,
-                    style = MaterialTheme.typography.h6
-                )
-            }
+        Row(
+            modifier = Modifier.animateContentSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            MealImage(meal = meal)
+            MealContent(meal = meal)
         }
     }
+}
+
+@Composable
+fun MealImage(meal: MealsResponse) {
+    AsyncImage(
+        model = meal.imageUrl,
+        contentDescription = null,
+        modifier = Modifier
+            .size(90.dp)
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun MealContent(meal: MealsResponse) {
+    var isExpanded by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(0.8f)
+    ) {
+        Text(
+            text = meal.name,
+            style = MaterialTheme.typography.h6
+        )
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = meal.description,
+                style = MaterialTheme.typography.subtitle2,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = if (isExpanded) 10 else 4,
+                textAlign = TextAlign.Start
+            )
+        }
+    }
+    Icon(
+        imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+        contentDescription = null,
+        modifier = Modifier
+            .clickable { isExpanded = !isExpanded }
+            .padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true)
